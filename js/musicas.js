27613884ae83719a5b6c61e2b748ad4d0648c9c8
Tales Carvalho <tales-h-carvalho@hotmail.com>
2022-03-01 20:13:37 -0700

    Day 2 fix (kinda)

--- js/musicas.js
+++ js/musicas.js
@@ -1,4 +1,4 @@
 const musicas = [
   {name: "Aerosmith - I Don't Want to Miss a Thing", id: "T-doQhcwDS8"},
-  {name: "Calma que to corrigindo isso, tá esquisito", id: "CvjN-9epBqA"}
+  {name: "Smash Mouth - All Star", id: "L_jWHffIx5E"}
 ]
